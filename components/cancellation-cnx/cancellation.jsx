900c2d524f50a219f1ad55ac893bfa92951d105f
rk2609 <accounts@wanderon.in>
2021-03-12 12:25:21 +0530

    Cnx Updated Cancellation

--- components/cancellation-cnx/cancellation.jsx
+++ components/cancellation-cnx/cancellation.jsx
@@ -4,18 +4,54 @@ import { Container, Heading } from "./cancellation.style";
 
 const Cancellation = () => (
   <Container>
+    <br />
     <Heading>Cancellation Policy for Concentrix Employees</Heading>
+    <br />
+    <strong>Booking Duration: 1-5 Days</strong>
     <ol>
       <li>
-        <strong>Less than 7 days</strong> from Check In Date:{" "}
-        <strong>A minimal amount</strong> will be charged as cancellation fees
-        and the rest of your amount will be adjusted for future bookings.
+        <strong>0-7 Days before Check-In:</strong> 15% of the Booking Value will
+        be charged as Cancellation Fees
       </li>
       <li>
-        <strong>7+ days</strong> from Check In Date:{" "}
-        <strong>Full Refund</strong> will be provided fees.
+        <strong>7+ Days before Check-In:</strong> Full Refund will be provided
       </li>
     </ol>
+    <br />
+    <strong>Booking Duration: 6-15 Days</strong>
+    <ol>
+      <li>
+        <strong>0-7 Days before Check-In:</strong> 30% of the Booking Value will
+        be charged as Cancellation Fees
+      </li>
+      <li>
+        <strong>8-15 Days before Check-In:</strong> 20% of the Booking Value
+        will be charged as Cancellation Fees
+      </li>
+      <li>
+        <strong>15+ Days before Check-In:</strong> Full Refund will be provided
+      </li>
+    </ol>
+    <br />
+    <strong>Booking Duration: 15+ Days</strong>
+    <ol>
+      <li>
+        <strong>0-7 Days before Check-In:</strong> 50% of the Booking Value will
+        be charged as Cancellation Fees
+      </li>
+      <li>
+        <strong>8-15 Days before Check-In:</strong> 30% of the Booking Value
+        will be charged as Cancellation Fees
+      </li>
+      <li>
+        <strong>15-30 Days before Check-In:</strong> 20% of the Booking Value
+        will be charged as Cancellation Fees
+      </li>
+      <li>
+        <strong>30+ Days before Check-In:</strong> Full Refund will be provided
+      </li>
+    </ol>
+    <br />
   </Container>
 );
 
--- components/cancellation-cnx/cancellation.style.jsx
+++ components/cancellation-cnx/cancellation.style.jsx
@@ -3,11 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 80%;
   margin: 20px auto;
-  height: calc(100vh - 280px);
 
   @media only screen and (max-width: 900px) {
     width: 90%;
-    height: 48vh;
   }
 
   li {
