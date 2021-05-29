c71ed63c1c490f68cf35e1feaae528c067f0efb4
r2609 <accounts@wanderon.in>
2021-05-29 20:24:37 +0530

    Internal Func

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import axios from "axios";
 
 import { Container, Heading } from "../../styles/properties/style";
 
 import Layout from "../../components/layout/layout";
 import PropertyListSearch from "../../containers/property-list-search/property-list-search";
+import { performSearch } from "../../utilities/search";
 
 const Properties = ({ maxPage, properties, search }) => {
-  const [page, setPage] = useState(1);
   const [list, setList] = useState([...properties]);
   const [isLoader, setIsLoader] = useState(true);
   const [pagesList, setPagesList] = useState(
@@ -28,19 +26,11 @@ const Properties = ({ maxPage, properties, search }) => {
         i === index ? "finished" : item
       );
 
-      const url = `https://api.workcations.in/search/${search}/${pageNo}`;
-      const config = {
-        method: "get",
-        url,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
       const slugsList = list.map((item) => item.item.slug);
 
-      axios(config)
+      performSearch(search, pageNo)
         .then((data) => {
-          const newList = data.data.list.filter(
+          const newList = data.list.filter(
             (item) => slugsList.indexOf(item.item.slug) === -1
           );
 
@@ -118,24 +108,12 @@ const Properties = ({ maxPage, properties, search }) => {
 };
 
 export const getServerSideProps = async ({ query: { search } }) => {
-  const url = `https://api.workcations.in/search/${search}/1`;
-
-  const config = {
-    method: "get",
-    url,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  const data = await axios(config).then((data) => {
-    return { maxPage: data.data.maxPage, properties: data.data.list };
-  });
+  const { maxPage, list } = await performSearch(search, 1);
 
   return {
     props: {
-      maxPage: data.maxPage,
-      properties: data.properties,
+      maxPage: maxPage,
+      properties: list,
       search: search,
     },
   };
---
+++ utilities/search.js
@@ -0,0 +1,100 @@
+import Fuse from "fuse.js";
+import { getProperties } from "./properties";
+
+const convertTitleState = (title, state) => {
+  const arr = title.split(" ");
+  let finalString = "" + arr[0] + " ";
+  for (let i = 1; i < arr.length - 1; i++) {
+    finalString += arr[i] + " ";
+  }
+  finalString += state;
+
+  return finalString;
+};
+
+const slugToString = (slug) => {
+  const arr = slug.split("-");
+  let finalString = "" + arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    finalString += " " + arr[i];
+  }
+  return finalString;
+};
+
+export const performSearch = async (query, page) => {
+  const list = await getProperties();
+
+  const displayList = list.map((property) => {
+    return {
+      titleState: convertTitleState(property.title, property.location.state),
+      slugString: slugToString(property.slug),
+      slugStringState: convertTitleState(
+        slugToString(property.slug),
+        property.location.state
+      ),
+      ...property,
+    };
+  });
+
+  const options = {
+    // isCaseSensitive: false,
+    includeScore: true,
+    // shouldSort: true,
+    // includeMatches: false,
+    // findAllMatches: false,
+    // minMatchCharLength: 1,
+    // location: 0,
+    // threshold: 0.6,
+    // distance: 100,
+    // useExtendedSearch: false,
+    // ignoreLocation: false,
+    // ignoreFieldNorm: false,
+    keys: [
+      {
+        name: "titleState",
+        weight: 2,
+      },
+      {
+        name: "slugStringState",
+        weight: 3,
+      },
+      {
+        name: "slugString",
+        weight: 3.1,
+      },
+      {
+        name: "slug",
+        weight: 0.1,
+      },
+      {
+        name: "title",
+        weight: 2.1,
+      },
+      {
+        name: "type",
+        weight: 1.2,
+      },
+      {
+        name: "location.city",
+        weight: 1.3,
+      },
+      {
+        name: "location.state",
+        weight: 1.4,
+      },
+      /*{
+        name: "filtersAll",
+        weight: 2,
+      },*/
+    ],
+  };
+
+  const fuse = new Fuse(displayList, options);
+
+  const FilteredList = fuse.search(query);
+
+  return {
+    list: FilteredList.filter((item, i) => i >= (page - 1) * 9 && i < page * 9),
+    maxPage: Math.ceil(FilteredList.length / 9, 0),
+  };
+};
